e8eecd9e9ae644449bd4ec2059e15714a07b9e12
Cláudio Meireles <claudioamj@hotmail.com>
2025-03-01 20:10:53 -0300

    .

--- Game/Scenes/characters/pessoas.gd
+++ Game/Scenes/characters/pessoas.gd
@@ -1,6 +1,26 @@
 extends RigidBody2D
 
 var started_position: Vector2
+@onready var timer: Timer = $Timer
 
 func _ready() -> void:
-	started_position = position
+	started_position = global_position
+	
+	
+func _physics_process(delta: float) -> void:
+	#if started_position != global_position:
+	pass
+		
+	
+		
+		
+
+
+func _on_timer_timeout() -> void:
+	var tween = get_tree().create_tween()
+	tween = tween.tween_property(self,"global_position",started_position,1)
+	print("foi")
+
+
+func _on_body_entered(body: Node) -> void:
+	timer.start()
