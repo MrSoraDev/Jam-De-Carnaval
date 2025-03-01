587735e0e1aabb403e2746fc4406053d7e9763d7
DASK <dagurasucontacts@gmail.com>
2025-03-01 20:26:04 -0300

    script folder

--- player.gd
+++ player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	
-	velocity = get_movement_axis() * SPEED
 	
+	velocity = get_movement_axis() * SPEED
 	move_and_slide()
 
 func get_movement_axis() -> Vector2:
@@ -25,3 +26,7 @@ func get_movement_axis() -> Vector2:
 
 	# Normalize the vector to prevent diagonal speed boost
 	return axis.normalized()
+
+func handle_animations():
+	if Input.is_action_just_pressed("action"):
+		animated_sprite_2d.play("dance1")
